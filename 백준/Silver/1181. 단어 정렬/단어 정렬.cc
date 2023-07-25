d5d63372bbcc4645a9c19f885f4a0636ac9cef35
ljh102715 <58131356+ljh102715@users.noreply.github.com>
2023-07-25 23:24:16 +0900

    [Silver V] Title: 단어 정렬, Time: 68 ms, Memory: 4528 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool compare(const string& a, const string& b){
+	if(a.length() == b.length()){
+		return a < b;
+	} else{
+		return a.length() < b.length();
+	}
+}
+
+int main(void) {
+	int num;
+	cin >> num;
+	vector<string> words;
+
+	for(int i = 0; i< num; i++){
+		string temp;
+		cin >> temp;
+		words.push_back(temp);
+	}
+
+	sort(words.begin(), words.end(), compare);
+	words.erase(unique(words.begin(), words.end()), words.end());
+	for(vector<string>::iterator iter = words.begin(); iter != words.end(); iter++){
+		if(*iter == words.back())
+			cout << *iter;
+		else
+			cout << *iter << "\n";
+	}
+}
