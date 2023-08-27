6458cdf6404b01e9c2887aa1e789d37f1b841f76
LEE JUNHYEOK <58131356+ljh102715@users.noreply.github.com>
2023-08-27 19:11:54 +0900

    [Silver V] Title: 행렬 곱셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.cc
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.cc
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(){
 
-	int a[100][100], b[100][100], arr[100][100] = {0,};
+	int a[100][100], b[100][100];
 
 	int n, m, k, result, col = 0;
 	cin >> n >> m;
@@ -18,28 +18,17 @@ int main(){
 	for(int i = 0; i < m; i++)
 		for(int j = 0; j < k; j++)
 			cin >> b[i][j];
-			
+
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < k; j++){
 			result = 0;
 			for(int l = 0; l < m; l++){
 				result += a[i][l] * b[l][j];
-
 			}
-			arr[i][j] = result;
-		}
-
-	}
-
-
-
-	for(int i = 0; i < n; i++){
-
-		for(int j = 0; j < k; j++){
-			cout << arr[i][j] << " ";
+			cout << result << " ";
 		}
 		cout << "\n";
-	}
 
+	}
 	return 0;
 }
