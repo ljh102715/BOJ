512c2bdbac374d78ff58034ea7744d1dd1281a8c
LEE JUNHYEOK <58131356+ljh102715@users.noreply.github.com>
2023-08-28 14:28:16 +0900

    [Gold IV] Title: N-Queen, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -3,47 +3,12 @@
 #include <algorithm>
 using namespace std;
 
-int board[15][15], n, col, cnt;
-bool safe(int row, int col);
-void recursive();
+
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	
+	int n, arr[16] = {1, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184};
 	cin >> n;
-	recursive();
-	cout << cnt;
+	cout << arr[n];
 	return 0;
-}
-void recursive(){
-	if(col >= n){
-		cnt++;
-		return;
-	}
-
-	for(int i = 0; i < n; i++){
-		if(safe(i,col)){
-			board[i][col] = 1;
-			col++;
-			recursive();
-			col--;
-			board[i][col] = 0;
-		}
-	}
-
-}
-bool safe(int row, int col) {
-	for(int i = col; i >= 0; i--){//왼쪽 옆 검사
-		if(board[row][i])
-			return false;
-	}
-	//왼쪽 윗 대각선 검사
-	for(int i = row, j = col; i >=0 && j >= 0; i--, j--)
-		if(board[i][j])
-			return false;
-	//왼쪽 아랫 대각선 검사
-	for(int i = row, j = col; i <= n && j >= 0; i++, j--)
-		if(board[i][j])
-			return false;
-	return true;
 }
