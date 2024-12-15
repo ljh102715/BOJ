b42bff446d2a0ee59e1a2bac4215a541179333f6
LEE JUNHYEOK <58131356+ljh102715@users.noreply.github.com>
2024-12-15 18:56:33 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 6.62 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,57 +1,23 @@
-import heapq 
+import heapq
 
-class Process:
-    
-    def __init__(self, rt, tt, index):
-        self.rt = rt
-        self.tt = tt
-        self.index = index
-        
-    def __lt__(self, other):
-        if self.tt != other.tt:
-            return self.tt < other.tt
-        if self.rt != other.rt:
-            return self.rt < other.rt
-        return self.index < other.index
-    
-    def __str__(self):
-        return "{} {} {}".format(self.rt, self.tt, self.index)
-    
 def solution(jobs):
     answer = 0
-    time = 0
-    cnt = 0
-    proccess_st = {}
+    hq = []
+    before, time, i = -1, 0, 0
     
-    for i in range(0, len(jobs)):
-        new_one = Process(jobs[i][0], jobs[i][1], i)
-        if jobs[i][0] in proccess_st.keys():
-            proccess_st[jobs[i][0]].append(new_one)
+    jobs.sort()
+    while i < len(jobs):
+        for job in jobs[i:]:
+            if before < job[0] <= time:
+                heapq.heappush(hq, [job[1], job[0]])
+        if hq:
+            p = heapq.heappop(hq)
+            print(p[1], p[0])
+            before = time
+            time += p[0]
+            answer += time - p[1]
+            i += 1
         else:
-            proccess_st[jobs[i][0]] = [new_one]
-    
-    time = min(proccess_st.keys())
-    proccess_heap = proccess_st[time]
-    heapq.heapify(proccess_heap)
-    del proccess_st[time]
-    
-    while cnt < len(jobs):
-        if not proccess_heap:
             time += 1
-        else: 
-            now = heapq.heappop(proccess_heap)
-            time += now.tt
-            answer += time - now.rt
-            cnt += 1
-        added = []
-        if proccess_st:
-            print(proccess_st.keys())
-            for each in proccess_st.keys():
-                if time >= each:
-                    added.append(each)
-                    for proccess in proccess_st[each]:
-                        heapq.heappush(proccess_heap, proccess)
-            for each in added:
-                del proccess_st[each]
-            
-    return answer // len(jobs)
+    
+    return answer//len(jobs)
